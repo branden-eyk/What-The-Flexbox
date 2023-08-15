cabfa3eaa62d517f80df47fd0ee735fdd14d2600
branden-eyk <brandeneyk@gmail.com>
2023-08-15 15:17:30 -0400

    Get autoprefixer working

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles',function(){
+gulp.task('styles', async function(){
     gulp.src('css/styles.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('build'))
